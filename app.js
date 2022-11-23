63b25ed580488a427a9d6b5191144fe3054eb5ee
unknown <zakie.nobakht@gmail.com>
2022-11-23 11:30:11 +0330

    main db, all collections

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const mongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+// const url = 'mongodb://localhost:27017';
+const url = 'mongodb://root:Password!123@192.168.238.10:27017';
 
 const dbName1 = '20220628';
+const tmpDB = 'tmpDB';
+const finalDB = 'newDB';
 let client;
 async function main() {
     try {
@@ -10,52 +13,43 @@ async function main() {
 
         const db = client.db(dbName1);
         const collections = await db.listCollections().toArray();
+        // console.log(collections);
 
-        const newDb = client.db("new1");
+        let time = new Date();
+        console.log(time.getMinutes())
 
-
-        // for (i = 0; i < collections.length; i++) {
-        for (i = 0; i < 2; i++) {
+        for (i = 0; i < collections.length; i++) {
+        // for (let i = 0; i < 2; i++) {
             let collection = db.collection(collections[i].name);
             await collection.aggregate([
                 { $project: { fieldName: `${collections[i].name}`, value: 1, name: 1, ts: 1, ProtocolCause: 1, 'Procedure identification': 1, _id: 0 } },
                 { $sort: { ProtocolCause: 1, 'Procedure identification': 1 }},
-                // { $group: { _id: { ProtocolCause: "$ProtocolCause", 'Procedure identification': "$Procedure identification" }, fieldName: { $first: '$fieldName' }, value: { $first: '$value' }, name: { $first: '$name' } }},
-                // { $merge: { into: { db: "new", coll: "$fieldName"}} }
-                // { $merge: "aggregation" }
-                // { $out: "$fieldName" }
-                { $out: { db: "new", coll: "aggr" } }
-                // {$out: "aggr"}
-                // { $out: { db: "new", coll: { $getField: "ProtocolCause" } }  }
-                // { $out: { $getField: "ProtocolCause"}}
+                { $out: { db: tmpDB, coll: "aggr" } }
             ]).toArray();
-            let output = await client.db("new").collection("aggr").aggregate([
+            let outputOfAggr = await client.db(tmpDB).collection("aggr").aggregate([
                 { $group: { _id: { ProtocolCause: "$ProtocolCause", ProcedureIdentification: "$Procedure identification" } } },
             ]).toArray();
 
-            const totalSize = await client.db("new").collection("aggr").count();
+            const totalSize = await client.db(tmpDB).collection("aggr").count();
             console.log(totalSize);
 
-            // let j = 0;
-            // let flag = 0;
-            for (i = 0; i < output.length; i++) {
+            for (let j = 0; j < outputOfAggr.length; j++) {
                 // for(i = 0; i<1; i++){
-                let name = `${output[i]._id.ProcedureIdentification}_${output[i]._id.ProtocolCause}`
-                //    await newDb.createCollection(name);
-                const outs = await client.db("new").collection("aggr").aggregate([
+                let name = `${outputOfAggr[j]._id.ProcedureIdentification}_${outputOfAggr[j]._id.ProtocolCause}`;
+                const outs = await client.db(tmpDB).collection("aggr").aggregate([
                     {
                         $match: {
-                            ProtocolCause: `${output[i]._id.ProtocolCause}`, "Procedure identification": `${output[i]._id.ProcedureIdentification}`
+                            ProtocolCause: `${outputOfAggr[j]._id.ProtocolCause}`, "Procedure identification": `${outputOfAggr[j]._id.ProcedureIdentification}`
                         }
                     },
-                    { $merge: { into: { db: "new1", coll: name } } }
+                    { $merge: { into: { db: finalDB, coll: name }, whenMatched: "keepExisting" } }
                 ]).toArray()
 
             }
-            console.log(i)
         }
-        
-        // console.log(output);
+
+        time = new Date();
+        console.log(time.getMinutes())
     } catch (err) {
         console.error(err)
     }
