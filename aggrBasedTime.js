c6ff51c12e8518997a516944dc09ddb9269f42f5
unknown <zakie.nobakht@gmail.com>
2022-12-10 10:35:49 +0330

    the code uses ts field to get year month day and ...., so each doc will have new fields : year, month etc. then, save them in a new collection

---
+++ aggrBasedTime.js
@@ -0,0 +1,53 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://root:Password!123@192.168.238.10:27017";
+const dbName = "20220628";
+const collectionName = "cache_Inner cause";
+const tmpDB = 'tmpDB1';
+
+let client;
+async function main(){
+    client = await MongoClient.connect(url);
+    console.log(`Connected to db`);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    await collection.aggregate([
+        {
+            $match:{
+                'Inner cause': 'Unknown'
+            }
+        },
+        {
+            $project:
+            {
+                year: { $year: "$ts" },
+                month: { $month: "$ts" },
+                day: { $dayOfMonth: "$ts" },
+                hour: { $hour: "$ts" },
+                'Inner cause':1,
+                value: 1, name: 1, ts: 1, ProtocolCause: 1, 'Procedure identification': 1,
+            }
+        },
+        {
+            $out:{
+                db: tmpDB, coll: "aggrTime"
+            }
+        }
+    ]).toArray();
+
+
+}
+
+let time = new Date();
+console.log(time.getMinutes())
+
+main()
+    .then(()=>{
+        client.close();
+        time = new Date();
+        console.log(time.getMinutes())
+    })
+    .catch(err=>{
+        console.log(err);
+        console.error(err);
+        client.close();
+    })
