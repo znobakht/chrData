edd1c201fb4368490f154f3736d918f710c3f5f1
unknown <nobakht9010@yahoo.com>
2023-01-17 15:57:34 +0330

    it saves unique values of 5 fields for filteration in grafana

---
+++ filter.js
@@ -0,0 +1,64 @@
+const mongoClient = require("mongodb").MongoClient;
+const dbUrl = "mongodb://root:Password!123@192.168.238.10:27017";
+
+const dbNameTemplate = "2022120"
+
+const finalDB = 'filters';
+const finalCollectionName = "filter";
+
+let client;
+async function main(){
+    try{
+        client = await mongoClient.connect(dbUrl);
+        console.log("connected to db");
+
+        // const finalCollection = client.db(finalDB).collection(finalCollectionName);
+        // for(let i =5; i<8; i++){
+        for(let i =6; i<8; i++){
+            console.log(new Date().getHours());
+            console.log(new Date().getMinutes());
+            console.log(i)
+
+            let dbName = `${dbNameTemplate}${i}`;
+            const collection = client.db(dbName).collection("chr");
+
+            const nodeName = "ESKZA";
+            const forNameOfCollections = await collection.aggregate([
+                {$group:{
+                        _id:{
+                            ChrType :"$ChrType",
+                            AccessType: "$AccessType",
+                            ProtocolCause: "$ProtocolCause", 
+                            ProcedureIdentification: "$Procedure identification",
+                        },
+                    }
+                },
+                {$project:{
+                    ChrType : "$_id.ChrType",
+                    AccessType : "$_id.AccessType",
+                    ProtocolCause : "$_id.ProtocolCause",
+                    ProcedureIdentification : "$_id.ProcedureIdentification",
+                    nodeName: nodeName
+                }},
+                {$merge:{ into:{
+                        db: finalDB, coll: finalCollectionName
+                    }, whenMatched: "keepExisting"}}
+            ], { "allowDiskUse": true }).toArray();
+        }
+        
+
+        await client.close();
+
+    }
+    catch(err){
+        console.error(err);
+        await client.close();
+    }
+}
+main()
+    .then(()=>console.log(new Date().getHours()))
+    .then(()=>console.log(new Date().getMinutes()))
+    .catch(err=>{
+        console.log(new Date().getMinutes());
+        console.error(err);
+    })
